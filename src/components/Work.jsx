d099cb759a55310a5379ddf1a498d2301bb35fd7
Negil Babu <negil.babu@innovaturelabs.com>
2023-12-27 18:05:13 +0530

    fix : exp

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,18 +3,18 @@ import WorkItem from "./WorkItem";
 
 const data = [
   {
-    year: 2022,
-    title: "Associate Software Engineer - Innovature Labs",
-    duration: "1 year",
-    details:
-      "Developed and maintained software applications using cutting-edge technologies, including Angular for frontend development, and Java with Spring Boot as well as Python with Django for backend development. Collaborated with a dynamic team on three diverse projects, showcasing expertise in Angular, Java-Spring Boot, and Python-Django. Actively participated in agile ceremonies, emphasizing teamwork, communication, and problem-solving skills to ensure the successful delivery of projects. Implemented RESTful APIs in Java-Spring Boot, resulting in improvement in data exchange efficiency. Stayed up-to-date with current technology trends, attending relevant courses to enhance skills and bring innovation to project solutions.",
-  },
-  {
     year: 2023,
     title: "Software Engineer - Innovature Labs",
-    duration: "3 months",
+    duration: "16 months",
     details:
       "Elevating my role as a Software Engineer, I led key initiatives at Innovature Labs in 2023. Over a brief short span, I adeptly navigated dynamic project landscapes, ensuring timely delivery and impeccable software quality. Spearheading the implementation of RESTful APIs, I optimized data exchange with external systems, showcasing a commitment to efficiency. Actively participating in agile ceremonies, I maintained an agile mindset for seamless project execution. My responsibilities spanned the entire software development lifecycle, emphasizing a holistic approach from analysis to deployment. This tenure exemplifies my adaptability, proficiency, and unwavering dedication to crafting high-quality, innovative solutions.",
+  },
+  {
+    year: 2022,
+    title: "Software Engineer Intern - Soften Technologies",
+    duration: "6 months",
+    details:
+      "As a Software Engineer Intern at Soften Technologies, I actively contributed to project success by collaborating with stakeholders, resulting in a 15% improvement in accurate project deliverability timeframe estimation. I played a key role in maintaining and adapting existing web applications, demonstrating proficiency in handling technical issues. Additionally, I shadowed team members, achieving a 10% faster onboarding for new tasks through enhanced problem-solving skills.My responsibilities spanned the entire software development lifecycle, emphasizing a holistic approach from analysis to deployment. This tenure exemplifies my adaptability, proficiency, and unwavering dedication to crafting high-quality, innovative solutions.",
   },
 ];
 
